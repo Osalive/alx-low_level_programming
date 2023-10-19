046d0cc9354d0ce06070609defe5612a8aadd4cc
Osama <osamamoha6599@gmail.com>
2023-10-19 11:30:45 -0700

    push header file

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -3,15 +3,20 @@
 
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
-int _strlen(char *s);
+int _strcmp(char *s1, char *s2);
 void _puts(char *str);
-void print_rev(char *s);
+void print_number(int n);
 void rev_string(char *s);
 void puts2(char *str);
-void puts_half(char *str);
-void print_array(int *a, int n);
-char *_strcpy(char *dest, char *src);
+void print_buffer(char *b, int size);
+void reverse_array(int *a, int n);
+char *_strncpy(char *dest, char *src, int n);
+char *_strncat(char *dest, char *src, int n);
+char *_strcat(char *dest, char *src);
+char *string_toupper(char *a);
+char *leet(char *a);
 int _putchar(char c);
-int _atoi(char *s);
+char *rot13(char *s);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
 
 #endif
