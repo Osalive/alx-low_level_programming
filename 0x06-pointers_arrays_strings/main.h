5327f52620a4cecaa53e87a655be2942cb288b15
Osama <osamamoha6599@gmail.com>
2023-10-20 11:55:16 -0700

    main.h

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,23 +1,17 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef HEADER_FILE
+#define HEADER_FILE
 
-void reset_to_98(int *n);
-void swap_int(int *a, int *b);
-int _strcmp(char *s1, char *s2);
-void _puts(char *str);
-void print_number(int n);
-void rev_string(char *s);
-void puts2(char *str);
-void print_buffer(char *b, int size);
-void reverse_array(int *a, int n);
-char *_strncpy(char *dest, char *src, int n);
-char *_strncat(char *dest, char *src, int n);
-char *_strcat(char *dest, char *src);
-char *string_toupper(char *a);
-char *leet(char *a);
 int _putchar(char c);
-char *rot13(char *s);
-char *infinite_add(char *n1, char *n2, char *r, int size_r);
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);
+char *string_toupper(char *);
 char *cap_string(char *);
+char *leet(char *);
+char *rot13(char *);
+char *_strstr(char *haystack, char *needle);
+void print_number(int n);
 
 #endif
