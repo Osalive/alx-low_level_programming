8af35969b7ce729834d49e94d76ae573b7fd4ad8
Osama <osamamoha6599@gmail.com>
2023-11-07 10:12:11 -0800

    init dog

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 #include "dog.h"
 
+/**
+* init_dog - inits a variable of
+*type dog
+*@d: dog identification
+*@name: name of dog
+*@age: age of dog
+*@owner: owner's name
+*/
+
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d != NULL)
