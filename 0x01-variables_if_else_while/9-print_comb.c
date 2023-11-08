fb209e2a8b81da45f1272b1e50b06e09d9c832f5
Osama <osamamoha6599@gmail.com>
2023-11-08 06:58:50 -0800

    combo

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - the main function
+ * for - loop that holds the numbers
+ * if - statement to target number 9
+ * putchar -prints whats in the loop
+ * Return: always 0 (success)
+ */
+
+int main(void)
+{
+int num;
+
+for (num = '0'; num <= '9'; num++)
+{
+putchar(num);
+if (num == 9)
+{
+putchar(',');
+putchar(' ');
+}
+}
+
+putchar('\n');
+return (0);
+}
