458c567694c751abb48de0b9b24dcb75d20a6aab
Osama <osamamoha6599@gmail.com>
2023-11-08 02:14:49 -0800

    print alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - the main function
+ * for loop - to list the letters in alphabetical order
+ * putchar - prints out what is inside letters
+ * Return: always 0 (success)
+ */
+
+int main(void)
+{
+char lower;
+char upper;
+
+	for (lower = 'a'; lower <= 'z'; lower++)
+{
+putchar(lower);
+}
+
+putchar('\n');
+
+	for (upper = 'A'; upper <= 'Z'; upper++)
+{
+putchar(upper);
+}
+
+    return (0);
+}
