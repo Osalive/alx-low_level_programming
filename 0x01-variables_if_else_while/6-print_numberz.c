b04604d45c71beecffa7047b27e7e1cbc874954b
Osama <osamamoha6599@gmail.com>
2023-11-08 05:57:12 -0800

    print numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - the main function
+ * for loop - lists the numbers of single digit
+ * putchar - provides new line
+ * Return: always 0 (success)
+ */
+
+int main(void)
+{
+int singledigit;
+
+for (singledigit = 0; singledigit <= 9; singledigit++)
+{
+putchar(singledigit);
+}
+putchar('\n');
+return (0);
+}
