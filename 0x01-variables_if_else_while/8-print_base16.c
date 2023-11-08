8686142f978ee73f3bd49d3643a083c31ba57c9a
Osama <osamamoha6599@gmail.com>
2023-11-08 06:34:50 -0800

    base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - the main function
+ * for loop - loop for the numbers of base 16
+ * putchar - prints out the base 16 numbers
+ * Return: always 0 (success)
+ */
+
+int main(void)
+{
+int num;
+char letter;
+
+for (num = '0'; num <= '9'; num++)
+{
+putchar(num);
+}
+
+for (letter = 'A'; letter <= 'F'; letter++)
+{
+putchar(letter);
+}
+putchar('\n');
+return (0);
+}
