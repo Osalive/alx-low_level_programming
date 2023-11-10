cb7e1e3e9b7516a4b9fccf487087bd00e42eaf4d
Osama <osamamoha6599@gmail.com>
2023-11-10 06:31:25 -0800

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - the main function
+ * putchar - prints out the output of the variable
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+char p = '_putchar';
+putchar(p);
+putchar('\n');
+
+return (0);
+}
