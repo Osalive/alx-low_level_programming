ab0e8f32c1f03d634908761c7cd30ba5a49a23c1
Osama <osamamoha6599@gmail.com>
2023-11-13 22:19:27 -0800

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,13 +3,17 @@
 /**
  * main - the main function
  * putchar - prints out the output of the variable
+ * for loop - increases i to 8 characters
  * Return: always 0 (success)
  */
 int main(void)
 {
-char p = '_putchar';
-putchar(p);
+char p[8] = "_putchar";
+int i;
+for (i = 0; i < 8; i++)
+{
+putchar(p[i]);
+}
 putchar('\n');
-
 return (0);
 }
