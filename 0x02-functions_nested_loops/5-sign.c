f1b181107064ac4652d1ac48ab36abf104264334
Osama <osamamoha6599@gmail.com>
2023-11-14 02:08:41 -0800

    sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * print_sign - to print the sign of a number
+ * @n: int type of variable
+ * Return: 1 if positive, 0 is zero, -1 if negative
+ * prints: + if positive, 0 if zero, - if negative
+ */
+
+int print_sign(int n)
+{
+if (n > 0)
+{
+putchar('+');
+return (1);
+}
+else if (n < 0)
+{
+putchar('-');
+return (-1);
+}
+else
+putchar('0');
+return (0);
+}
